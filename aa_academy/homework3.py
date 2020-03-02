f74f5adef9b51d99de40543ec2f0801be0f6079a
Anastasiya Nikonorova <aan@avito.ru>
2020-03-02 23:46:43 +0300

    edit

--- aa_academy/homework3.py
+++ aa_academy/homework3.py
@@ -1,13 +1,14 @@
 import string
 import numpy
 
-def display_field(field: list, FIELD_SIZE: int):
+
+def display_field(field: list, field_size: int):
     """ Визуализация игрового поля """
-    for i in range(FIELD_SIZE):
-        print('|'+HORIZONTAL[i], end='|')
+    for i in range(field_size):
+        print('|'+horizontal[i], end='|')
     print()
-    for i in range(FIELD_SIZE):
-        for j in range(FIELD_SIZE):
+    for i in range(field_size):
+        for j in range(field_size):
             print('|'+field[i][j], end='|')
         print(str(i+1))
 
@@ -20,7 +21,7 @@ def error():
 def win_diagonal1(field: list) -> bool:
     """ Проверка победы по главной диагонали """
     diag_count = 0
-    for i in range(FIELD_SIZE):
+    for i in range(field_size):
         check = field[0][0]
         if field[i][i] == check and check != ' ':
             diag_count += 1
@@ -34,7 +35,7 @@ def win_diagonal2(field: list) -> bool:
     diag_count = 0
     new_field = field.copy()
     new_field.reverse()
-    for i in range(FIELD_SIZE):
+    for i in range(field_size):
         check = new_field[0][0]
         if new_field[i][i] == check and check != ' ':
             diag_count += 1
@@ -68,6 +69,7 @@ def count_empty(field: list) -> int:
     count_empty = 0
     for line in field:
         count_empty += line.count(' ')
+
     return count_empty
 
 
@@ -75,7 +77,8 @@ def step(field: list) -> str:
     """ Шаги игроков до победы либо до заполнения поля """
     step_coord = []
     step_num = 0
-    while count_empty(field) > 0:
+    someone_won = False
+    while step_num < field_size ** 2 and not someone_won:
         if not step_num % 2:
             player_num = 1
             sign = 'X'
@@ -85,41 +88,38 @@ def step(field: list) -> str:
         print('Ход игрока ' + str(player_num) + '. Введите номер ячейки:')
         step = input()
         step_coord = list(step)
-        if step_coord[0] in HORIZONTAL:
-            h_field = HORIZONTAL.index(step_coord[0])
-            if int(step_coord[1]) in VERTICAL:
-                v_field = int(step_coord[1])-1
-                if field[v_field][h_field] == ' ':
-                    field[v_field][h_field] = sign
-                    step_num += 1
-                    if win_horizontal(field):
-                        print('Выиграл игрок ' + str(player_num))
-                        break
-                    if win_vertical(field):
-                        print('Выиграл игрок ' + str(player_num))
-                        break
-                    if win_diagonal1(field):
-                        print('Выиграл игрок ' + str(player_num))
-                        break
-                    if win_diagonal2(field):
-                        print('Выиграл игрок ' + str(player_num))
-                        break
-                else:
-                    error()
-            else:
-                error()
-        else:
+        column, row = step
+
+        if column not in horizontal:
             error()
+            continue
 
-        print(display_field(field, FIELD_SIZE))
+        if int(row) not in vertical:
+            error()
+            continue
 
-    print('Ничья')
+        h_field = horizontal.index(column)
+        v_field = int(row)-1
+
+        if field[v_field][h_field] != ' ':
+            error()
+            continue
+
+        field[v_field][h_field] = sign
+        step_num += 1
+        someone_won = win_horizontal(field) or win_vertical(field) or win_diagonal1(field) or win_diagonal2(field)
+        if someone_won:
+            print('Выиграл игрок ' + str(player_num))
+
+        print(display_field(field, field_size))
+    if not someone_won:
+        print('Ничья')
 
 if __name__ == '__main__':
     print('Укажите размер поля от 3 до 5:')
-    FIELD_SIZE = int(input())
-    HORIZONTAL = list(string.ascii_lowercase[:FIELD_SIZE])
-    VERTICAL = [i + 1 for i in range(FIELD_SIZE)]
-    field = [[' '] * FIELD_SIZE for i in range(FIELD_SIZE)]
-    display_field(field, FIELD_SIZE)
+    field_size = int(input())
+    horizontal = list(string.ascii_lowercase[:field_size])
+    vertical = range(1, field_size+1)
+    field = [[' '] * field_size for i in range(field_size)]
+    display_field(field, field_size)
     step(field)
